74510419b54a831164d97b3bf07cb56ec1405d9a
kawk10 <wonyeongwonyeong@gmail.com>
2024-05-26 16:07:37 +0900

    +

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const img = ["../img/DMZ1.png","../img/DMZ2.jpg"]
+let i = 0
+document.getElementById('goLeft').addEventListener('click', function() {
+    const imageContainer = document.getElementById('imageContainer');
+    i = (i - 1 + img.length) % img.length;
+    imageContainer.style.backgroundImage = `url(${img[i]})`;
+
+});
+document.getElementById('goRight').addEventListener('click', function() {
+    const imageContainer = document.getElementById('imageContainer');
+    i = (i + 1) % img.length;
+    imageContainer.style.backgroundImage = `url(${img[i]})`;
+
+});
