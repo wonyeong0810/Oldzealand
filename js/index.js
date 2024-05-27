bfac5a13252b79fb1eedcf46ab30b0ed7937cdac
kawk10 <wonyeongwonyeong@gmail.com>
2024-05-27 11:05:19 +0900

    +

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const img = ["../img/DMZ1.png","../img/DMZ2.jpg"]
+const img = ["../img/DMZ1.png","../img/DMZ2.jpg","../img/DMZ3.jpg", "../img/DMZ4.jpg"]
 let i = 0
 document.getElementById('goLeft').addEventListener('click', function() {
     const imageContainer = document.getElementById('imageContainer');
